a816088d639cb6e13d970216e2ecb1953faca20a
Kanatanagano <natakana2525@gmail.com>
2024-06-14 18:49:24 +0900

    Sidebarを追加jsなし

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 import Main from './component/Main';
 import Sidebar from './component/Sidebar';
 
+
 function App() {
+
+  const [notes, setNotes] = useState([]);
+  const onAddNote = () => {
+    console.log("ノートを追加しました")
+    const newNote = {
+      id: 1,
+      title: "新しいノート",
+      content: "新しいノートの内容",
+      moddate: Date.now()
+    };
+    setNotes([...notes, newNote]);
+    console.log(notes)
+  };
+
   return (
     <div className='App'>
-      <Sidebar />
+      <Sidebar onAddNote={onAddNote} notes={notes}/>
       <Main />
     </div>
   );
